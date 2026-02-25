48b40ac91e4b50428b7b14887d3d91bb69d7d00a
EvgeniyCig <j548@ya.ru>
2026-02-25 23:00:04 +0300

    Create minimal Vue single-page project

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="page">vueTest</main>
+</template>
+
+<style>
+:root {
+  color-scheme: dark;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+html,
+body,
+#app {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+
+.page {
+  display: grid;
+  place-items: center;
+  min-height: 100%;
+  background: #000;
+  color: #fff;
+  font-family: Arial, sans-serif;
+  font-size: 3rem;
+  font-weight: 700;
+}
+</style>
